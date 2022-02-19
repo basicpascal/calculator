6e284820674adac4238db610e02bdd7bcf87cfe2
Danila Kalugin <danilakalugin@yandex.ru>
2022-02-19 12:46:20 +0300

    Добавил кнопки управления калькулятором

--- main.py
+++ main.py
@@ -4,6 +4,35 @@ from tkinter import *
 class Main(Frame):
     def __init__(self, root):
         super(Main, self).__init__(root)
+        self.build()
+
+    def build(self):
+        btns = [
+            "C", "DEL", "*", "=",
+            "1", "2", "3", "/",
+            "4", "5", "6", "+",
+            "7", "8", "9", "-",
+            "+/-", "0", "√x", "x²"
+        ]
+        x = 10
+        y = 140
+        for bt in btns:
+            com = lambda x=bt: self.logicalc(x)
+            Button(text=bt, bg="#FFF",
+                   font=("Times New Roman", 15),
+                   command=com).place(x=x, y=y,
+                                      width=115,
+                                      height=79)
+            x += 117
+            if x > 400:
+                x = 10
+                y += 81
+
+    def logicalc(self, operation):
+        pass
+
+    def update():
+        pass
 
 
 if __name__ == '__main__':
